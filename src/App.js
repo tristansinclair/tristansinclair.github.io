d5d701fda48eb06c20d76c6e0ef6b5297c4629a1
tristansinclair <67209026+tristansinclair@users.noreply.github.com>
2022-02-07 20:01:43 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+
+function IntroPage() {
+  return (
+    <div className='Intro-Screen'>
+      <span className='Title'>Tristan Sinclair</span>
+    </div>
+
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IntroPage />
     </div>
+
+
+
   );
 }
+
 
 export default App;
