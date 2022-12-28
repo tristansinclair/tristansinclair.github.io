f3155b5d1b140b493b0a04c11f96334e7b64515a
tristansinclair <67209026+tristansinclair@users.noreply.github.com>
2022-12-28 05:09:08 +0000

    Deploying to gh-pages from @ tristansinclair/tristansinclair.github.io@07824331f3c8402f493a1dbb9591f4552540b936 🚀

---
+++ _next/static/Zyheq54hzgbUWSjvO3SX2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/907-27121c7f1b456dfe.js","static/chunks/pages/index-50e484ec20d312ac.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
