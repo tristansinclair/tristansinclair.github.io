48dc6a552dc6cffb75e99c47bc19d83f55e8ed2f
tristansinclair <67209026+tristansinclair@users.noreply.github.com>
2023-01-27 12:30:01 -0800

    advertising company section

--- pages/tldr/advertising.tsx
+++ pages/tldr/advertising.tsx
@@ -1,4 +1,20 @@
 import Head from "next/head";
+import Image from "next/image";
+import {
+  BanknotesIcon,
+  FingerPrintIcon,
+  RocketLaunchIcon,
+  UsersIcon,
+} from "@heroicons/react/24/outline";
+import NavLayout from "../../components-tldr/NavLayout";
+import clsx from "clsx";
+
+import logoAWS from "../../images/tldr/logos/aws.svg";
+import logoAzure from "../../images/tldr/logos/azure.svg";
+import logoCloudflare from "../../images/tldr/logos/cloudflare.svg";
+import logoLaunchDarkly from "../../images/tldr/logos/launchdarkly.svg";
+import logoRetool from "../../images/tldr/logos/retool.svg";
+import logoDrata from "../../images/tldr/logos/drata.svg";
 
 Advertise.getLayout = function getLayout(page: any) {
   return (
@@ -18,23 +34,77 @@ export default function Advertise() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="relative py-0 pb-12 lg:py-24 lg:px-12">
+      <main className="relative py-0 pb-12 lg:px-12">
         <div className="relative mx-auto w-full px-10 lg:max-w-6xl lg:px-8">
-          <WhySection />
+          <CompanySection />
+          <WhySection />ƒ
         </div>
       </main>
     </>
   );
 }
 
-import {
-  BanknotesIcon,
-  FingerPrintIcon,
-  RocketLaunchIcon,
-  UsersIcon,
-} from "@heroicons/react/24/outline";
-import NavLayout from "../../components-tldr/NavLayout";
-import clsx from "clsx";
+function CompanySection() {
+  let companies = [
+    {
+      name: "AWS",
+      logo: logoAWS,
+      style: "object-contain",
+    },
+    {
+      name: "Cloudflare",
+      logo: logoCloudflare,
+      style: "object-contain",
+    },
+    {
+      name: "Launch Darkly",
+      logo: logoLaunchDarkly,
+      style: "object-contain",
+    },
+    {
+      name: "Retool",
+      logo: logoRetool,
+      style: "object-contain",
+    },
+    {
+      name: "Azure",
+      logo: logoAzure,
+      style: "object-contain md:col-start-2",
+    },
+    {
+      name: "Drata",
+      logo: logoDrata,
+      style: "object-contain md:col-start-3",
+    },
+  ];
+  return (
+    <div className="py-12">
+      <h2 className="text-center text-4xl font-bold tracking-tight text-zinc-700 dark:text-zinc-200 sm:text-4xl">
+        The world's most innovative companies advertise on our newsletter.
+      </h2>
+      <div className="py-12">
+        <div className="grid w-full grid-cols-2 justify-items-stretch gap-8  md:grid-cols-4">
+          {companies.map((company) => (
+            <div
+              key={company.name}
+              className={clsx(
+                "relative col-span-1 h-12 stroke-red-300 md:h-16",
+                company.style
+              )}
+            >
+              <Image
+                src={company.logo}
+                alt={company.name}
+                className={clsx("dark:logo-dark", company.style)}
+                fill
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 const features = [
   {
