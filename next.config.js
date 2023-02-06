0eef185e2cbff477850db9b23b1f78538e95f158
tristansinclair <67209026+tristansinclair@users.noreply.github.com>
2023-02-05 20:43:37 -0800

    hot fix: build issues

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      "/": { page: "/", query: { __nextDefaultLocale: "en" } },
+      "/": { page: "/" },
     };
   },
   images: {
